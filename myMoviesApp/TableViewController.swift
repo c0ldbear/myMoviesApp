1ea6eeb769eb9b7c333c2ce36f28dd9545df2613
teddy <teddyjh@protonmail.com>
2022-08-31 10:52:24 +0200

    Add simple sample data to display in table

--- myMoviesApp/TableViewController.swift
+++ myMoviesApp/TableViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class TableViewController: UITableViewController {
 
+    private var movies  = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if movies.isEmpty {
+            movies.append("Alien")
+            movies.append("Aliens")
+            movies.append("Predator")
+        }
+        
+        tableView.reloadData()
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Movie", for: indexPath)
+        cell.textLabel?.text = movies[indexPath.row]
+        return cell
+    }
+    
 }
 
