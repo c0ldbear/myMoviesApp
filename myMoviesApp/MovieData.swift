98d1c3b6ca983ffc03cf52da032f4ab851064ac6
teddy <teddyjh@protonmail.com>
2022-08-31 11:52:15 +0200

    Add movie data model

---
+++ myMoviesApp/MovieData.swift
@@ -0,0 +1,13 @@
+//
+//  MovieData.swift
+//  myMoviesApp
+//
+//  Created by teddy on 2022-08-31.
+//
+
+import Foundation
+
+struct MovieData: Codable, Hashable {
+    let id: String
+    let title: String
+}
