421b3d4bfd71bf1823de5c8d37ca7289c9680ba2
teddy <teddyjh@protonmail.com>
2022-08-31 11:44:44 +0200

    Add class ApiCaller & static var api key

---
+++ myMoviesApp/ApiCaller+ApiKey.swift
@@ -0,0 +1,14 @@
+//
+//  ApiCaller+ApiKey.swift
+//  myMoviesApp
+//
+//  Created by teddy on 2022-08-31.
+//
+
+import Foundation
+
+extension ApiCaller {
+    static var apiKey: String {
+        return "YOUR APIKEY HERE"
+    }
+}
---
+++ myMoviesApp/ApiCaller.swift
@@ -0,0 +1,17 @@
+//
+//  ApiCaller.swift
+//  myMoviesApp
+//
+//  Created by teddy on 2022-08-31.
+//
+
+import Foundation
+
+class ApiCaller {
+    
+    var imdbApiUrl: URL = URL(string: "https://imdb-api.com/en/API/MostPopularMovies/"+apiKey)!
+    
+    func fetch() async {
+        
+    }
+}
